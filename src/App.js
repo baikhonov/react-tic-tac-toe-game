4b521fb1db19a54a2491387fb5934c40e05f9a3d
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2026-06-12 18:27:36 +0500

    Реализовал вывод истории шагов игры

--- src/App.js
+++ src/App.js
@@ -66,6 +66,26 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start'
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
@@ -73,7 +93,7 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>
-          {/* TODO */}
+          {moves}
         </ol>
       </div>
     </div>
